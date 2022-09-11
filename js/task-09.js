2c1819ca7c71aaeff5996b0d6b5325c979eece73
Gector4 <gector4@gmail.com>
2022-09-11 17:32:59 +0200

    corrected task number 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,11 +1,13 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 const bodyEl = document.querySelector('body');
 const buttonEl = document.querySelector('.change-color');
 const colorEl = document.querySelector('.color');
 
 buttonEl.addEventListener('click', () => {
-  buttonEl.style.backgroundColor = getRandomHexColor();
-  colorEl.textContent = buttonEl.style.backgroundColor;
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  colorEl.textContent = bodyEl.style.backgroundColor;
 });
